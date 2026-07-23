a1c3528da94b351219339d872e86de75fa3fd7e6
elitesociety <elitesociety.sup@gmail.com>
2026-07-23 08:52:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 
-from telegram import Update
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
 from telegram.ext import (
     Application,
     CommandHandler,
+    CallbackQueryHandler,
     ContextTypes
 )
 
@@ -13,39 +19,72 @@ from keep_alive import keep_alive
 TOKEN = os.getenv("BOT_TOKEN")
 
 
-# وقتی کاربر /start میزند
+# Welcome Image
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    await update.message.reply_text(
-        """
-🕵️‍♂️ به Elite Society خوش آمدید
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "🎯 START MISSION",
+                callback_data="mission1"
+            )
+        ]
+    ]
 
-شما وارد یک پرونده محرمانه شده‌اید.
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-آماده باشید...
-اولین ماموریت شما به زودی آغاز می‌شود.
 
-🎯 برای دریافت ماموریت‌ها منتظر بمانید.
-        """
+    await update.message.reply_photo(
+        photo=open("welcome.jpg", "rb"),
+
+        caption="""
+🕵️ Welcome to Elite Society
+
+Your journey begins now.
+
+Are you ready to uncover the truth?
+        """,
+
+        reply_markup=reply_markup
     )
 
 
-# خطای نبودن توکن
-if not TOKEN:
-    print("ERROR: BOT_TOKEN not found!")
+
+# Mission button
+async def mission(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+
+    await query.answer()
+
+
+    if query.data == "mission1":
+
+        await query.message.reply_video(
+            video=open("mission1.mp4", "rb"),
+
+            caption="""
+🎯 MISSION #001
+
+Your first mission has started.
+
+Watch carefully.
+Every detail matters.
+
+Good luck.
+            """
+        )
+
 
 
 def main():
 
-    # فعال کردن وب سرور برای UptimeRobot
     keep_alive()
 
 
-    # ساخت ربات
     app = Application.builder().token(TOKEN).build()
 
 
-    # دستور start
     app.add_handler(
         CommandHandler(
             "start",
@@ -54,10 +93,16 @@ def main():
     )
 
 
-    print("Elite Society Bot Started!")
+    app.add_handler(
+        CallbackQueryHandler(
+            mission
+        )
+    )
 
 
-    # اجرای دائمی ربات
+    print("Elite Society Bot Started")
+
+
     app.run_polling()
 
 
