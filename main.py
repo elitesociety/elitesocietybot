11fb463f37ccd25bccb210bd8dcb6b4d4d86768c
elitesociety <elitesociety.sup@gmail.com>
2026-07-23 08:40:18 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes
+)
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    await update.message.reply_text(
+        "🕵️ Welcome to Elite Society\n\n"
+        "Your first mission is waiting..."
+    )
+
+
+def main():
+
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(
+        CommandHandler("start", start)
+    )
+
+    print("Bot Started")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
