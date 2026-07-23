0b9af0e6afe74690cc2067c9695c20ed78fcfbb7
elitesociety <elitesociety.sup@gmail.com>
2026-07-23 08:44:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,59 @@ from telegram.ext import (
     ContextTypes
 )
 
+from keep_alive import keep_alive
+
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 
+# وقتی کاربر /start میزند
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
-        "🕵️ Welcome to Elite Society\n\n"
-        "Your first mission is waiting..."
+        """
+🕵️‍♂️ به Elite Society خوش آمدید
+
+شما وارد یک پرونده محرمانه شده‌اید.
+
+آماده باشید...
+اولین ماموریت شما به زودی آغاز می‌شود.
+
+🎯 برای دریافت ماموریت‌ها منتظر بمانید.
+        """
     )
+
+
+# خطای نبودن توکن
+if not TOKEN:
+    print("ERROR: BOT_TOKEN not found!")
 
 
 def main():
 
+    # فعال کردن وب سرور برای UptimeRobot
+    keep_alive()
+
+
+    # ساخت ربات
     app = Application.builder().token(TOKEN).build()
 
+
+    # دستور start
     app.add_handler(
-        CommandHandler("start", start)
+        CommandHandler(
+            "start",
+            start
+        )
     )
 
-    print("Bot Started")
 
+    print("Elite Society Bot Started!")
+
+
+    # اجرای دائمی ربات
     app.run_polling()
+
 
 
 if __name__ == "__main__":
